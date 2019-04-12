9582d1702b843ae13f2e2ca637e7067a4f2ad1af
Anthony Anicotte <anthony.anicotte@epitech.eu>
2019-04-12 19:58:24 +0200

    renamed the header for the simulator

--- include/function_creator.h
+++ include/function_creator.h
@@ -12,9 +12,13 @@
 void free_tab(char **tab);
 void free_structure(creator_t *crea_struct);
 void print_tab(char **tab);
-void start_creator(creator_t *crea_struct);
+int start_creator(creator_t *crea_struct, int ac, char **av);
 void clear_print_and_refresh(creator_t *crea_struct);
 int there_is_a_dot_under_the_guy(creator_t *crea_struct);
+void automatic_gen_map(creator_t *crea_struct);
+int flag_r_option(creator_t *crea_struct, int ac, char **av);
+void create_new_dot(creator_t *crea_struct, int x, int y);
+void print_map(char *file_path, creator_t *crea_struct);
 
 void key_up(creator_t *crea_struct);
 void key_down(creator_t *crea_struct);
