685a5ce52c3fb77a41760ce097c78edaae216a29
Anthony Anicotte <anthony.anicotte@epitech.eu>
2019-04-13 03:11:56 +0200

    added some functions to the header

--- include/function_creator.h
+++ include/function_creator.h
@@ -12,13 +12,17 @@
 void free_tab(char **tab);
 void free_structure(creator_t *crea_struct);
 void print_tab(char **tab);
-int start_creator(creator_t *crea_struct, int ac, char **av);
+int start_creator(creator_t *crea_struct, char **av);
 void clear_print_and_refresh(creator_t *crea_struct);
 int there_is_a_dot_under_the_guy(creator_t *crea_struct);
 void automatic_gen_map(creator_t *crea_struct);
-int flag_r_option(creator_t *crea_struct, int ac, char **av);
 void create_new_dot(creator_t *crea_struct, int x, int y);
 void print_map(char *file_path, creator_t *crea_struct);
+int error_handling_creator(char **av);
+int my_str_isnum(char const *str);
+int search_flag_place(char **av, char flag);
+void init_struct(char **av, creator_t *crea_struct);
+void fill_map(char *file_path, creator_t *crea_struct);
 
 void key_up(creator_t *crea_struct);
 void key_down(creator_t *crea_struct);
